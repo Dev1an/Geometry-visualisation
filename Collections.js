a73c9d651b88c4d0b19ee08431a5c942cb3e6cb3
Devian <info@devian.tk>
2016-02-10 11:04:36 +0100

    Restructure app

---
+++ Collections.js
@@ -0,0 +1,17 @@
+Boxes = new Mongo.Collection('boxes', {
+	transform(box) {
+		// Merge the box geometry into the box object
+		Object.defineProperty(box, 'geometry', {
+			get() { return Geometries.findOne(box.geometryId) }
+		})
+
+		for (let object of box.objects) {
+			// Merge the object's geometry into the 'object' object
+			Object.defineProperty(object, 'geometry', {
+				get() { return Geometries.findOne(object.geometryId) }
+			})
+		}
+		return box
+	}
+})
+Geometries = new Mongo.Collection('geometries')
