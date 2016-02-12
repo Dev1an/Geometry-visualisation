6c9f2e440093e5cf49ea47432be474f08f63e719
Devian <info@devian.tk>
2016-02-12 14:37:26 +0100

    basic job implementation

--- client/d3Visualisation.js
+++ client/d3Visualisation.js
@@ -1,4 +1,4 @@
-const line = d3.svg.line()
+line = d3.svg.line()
 line.interpolate('linear-closed')
 var svgContainer
 
@@ -7,7 +7,7 @@ Template.body.onRendered(function() {
 	Tracker.autorun(function() {
 		svgContainer.classed('controlling', FlowRouter.getRouteName() == 'robot')
 	})
-	const boxPath = svgContainer.append('path').attr('class', 'box')
+	const boxPath = svgContainer.insert('path', '.arrow').attr('class', 'box')
 
 	Tracker.autorun(function() {
 		const box = Boxes.findOne()
@@ -15,7 +15,7 @@ Template.body.onRendered(function() {
 		if (box && box.geometry) {
 			const boxGeometry = box.geometry
 
-			svgContainer.attr('viewBox', viewBox(boxGeometry))
+			svgContainer.attr('viewBox', viewBox(boxGeometry).join(' '))
 
 			boxPath.attr('d', line(boxGeometry.coordinates))
 
@@ -26,24 +26,48 @@ Template.body.onRendered(function() {
 			geometryDefinitions.classed('object', true)
 
 			const objectGroups = svgContainer.selectAll('g').data(box.objects)
-			objectGroups.enter().append('g').classed('objects', true)
+			objectGroups.enter().insert('g', '.arrow').classed('objects', true)
 
 			const objectPaths = objectGroups.selectAll('use').data(function(data, primitiveIndex) {
 				const geometry = data.geometry
-				return data.instances.map((object, instanceIndex) => {
-					return {geometryId: data.geometryId, translation: object.position, primitiveIndex, instanceIndex, handled: object.handled, rotation: object.rotation[2]}
+				return data.instances.map((instance, instanceIndex) => {
+					return _.extend(instance, {geometryId: data.geometryId, primitiveIndex, instanceIndex})
 				})
 			})
 
 			objectPaths.enter().append('use')
 			objectPaths.attr('xlink:href', object => '#object' + object.geometryId)
-			objectPaths.attr('transform', object => `translate(${object.translation[0]} ${object.translation[1]}) rotate(${object.rotation/Math.PI*180} 30 40)`)
+			objectPaths.attr('transform', object => `translate(${object.position[0]} ${object.position[1]}) rotate(${object.rotation[2]/Math.PI*180} 30 40)`)
 			objectPaths.classed('handled', object => object.handled)
+			objectPaths.classed('handling', object => object.handling)
 			objectPaths.on('click', function(object){
 				Boxes.update(box._id, {$set: {
 					[`objects.${object.primitiveIndex}.instances.${object.instanceIndex}.handled`]: !object.handled
 				}})
 			})
+		}
+	})
+
+	// Move the arrow
+	const arrow = d3.select('path.arrow')
+	Tracker.autorun(function() {
+		const job = Jobs.findOne({}, {sort: {date: -1}})
+		if (job && job.boxId) {
+			const box = Boxes.findOne(job.boxId)
+			var primitiveCursor = job.object, instanceCursor = job.instance
+
+			if (instanceCursor < box.objects[primitiveCursor].instances.length-1) {
+				instanceCursor++
+			} else {
+				instanceCursor = 0
+
+				if (primitiveCursor < box.objects.length -1) primitiveCursor++
+				else primitiveCursor = 0
+			}
+
+			const object = box.objects[primitiveCursor]
+			const boxView = viewBox(object.geometry)
+			arrow.attr('transform', `translate(${boxView[0]+boxView[2]/2 - 13.5 + object.instances[instanceCursor].position[0]} ${boxView[1]+boxView[3]/2 - 18 + object.instances[instanceCursor].position[1]})`)
 		}
 	})
 })
@@ -62,5 +86,5 @@ function viewBox(polygon) {
 	const boxRight  = Math.max(...boxXCoordinates)
 	const boxBottom = Math.max(...boxYCoordinates)
 
-	return `${boxLeft} ${boxTop} ${Math.abs(boxRight-boxLeft)} ${Math.abs(boxBottom-boxTop)}`
+	return [boxLeft, boxTop, Math.abs(boxRight-boxLeft), Math.abs(boxBottom-boxTop)]
 }
--- client/robotSimulator.js
+++ client/robotSimulator.js
@@ -1,3 +1,9 @@
+var closed = true
+var primitiveCursor = 0
+var instanceCursor = -1
+
+var previousPalmPosition = [200,0,100]
+
 FlowRouter.route('/robot', {
 	name: "robot",
 	action(params, queryParams) {
@@ -5,21 +11,72 @@ FlowRouter.route('/robot', {
 
 		Tracker.autorun(function() {
 			const box = Boxes.findOne()
+
 			if (box) {
+
 				controller.removeAllListeners('animationFrame')
 				controller.on('animationFrame', function (frame) {
 					const hand = frame.hands[0]
 
 					if (hand) {
-						Boxes.update(box._id, {$set: {
-							"objects.0.instances.0.position": [hand.palmPosition[0]*5, hand.palmPosition[2]*5],
-							"objects.0.instances.0.rotation": [hand.pitch(), hand.roll(), hand.yaw()]
-						}})
+						if (!closed) {
+							Boxes.update(box._id, {$set: {
+								[`objects.${primitiveCursor}.instances.${instanceCursor}.position`]: [
+									(hand.palmPosition[0]-previousPalmPosition[0])*5 + box.objects[primitiveCursor].instances[instanceCursor].position[0],
+									(hand.palmPosition[2]-previousPalmPosition[2])*5 + box.objects[primitiveCursor].instances[instanceCursor].position[1]
+								],
+								[`objects.${primitiveCursor}.instances.${instanceCursor}.rotation`]: [hand.pitch(), hand.roll(), hand.yaw()]
+							}})
+							previousPalmPosition = hand.palmPosition
+						}
+
+						if (hand.grabStrength < 0.5 && closed) {
+							previousPalmPosition = hand.palmPosition
+							closed = false
+
+							advance(box)
+							Boxes.update(box._id, {$set: {
+								[`objects.${primitiveCursor}.instances.${instanceCursor}.handling`]: true
+							}})
+						} else if (hand.grabStrength > 0.5 && !closed) {
+							closed = true
+							Boxes.update(box._id, {$set: {
+								[`objects.${primitiveCursor}.instances.${instanceCursor}.handling`]: false
+							}})
+						}
 					}
 				})
 			}
+		})
+
+		var listener = new Keypress.Listener()
+		listener.simple_combo('space', function() {
+			const box = Boxes.findOne()
+			advance(box)
+			Boxes.update(box._id, {$set: {
+				[`objects.${primitiveCursor}.instances.${instanceCursor}.handling`]: true
+			}})
+			Meteor.setTimeout(function() {
+				Boxes.update(box._id, {$set: {
+					[`objects.${primitiveCursor}.instances.${instanceCursor}.handled`]: true,
+					[`objects.${primitiveCursor}.instances.${instanceCursor}.handling`]: false,
+				}})
+			}, 1800)
 		})
 	}
 });
 
 FlowRouter.route('/', {action() {}})
+
+function advance(box) {
+	if (instanceCursor < box.objects[primitiveCursor].instances.length-1) {
+		instanceCursor++
+	} else {
+		instanceCursor = 0
+
+		if (primitiveCursor < box.objects.length -1) primitiveCursor++
+		else primitiveCursor = 0
+	}
+
+	Jobs.insert({boxId: box._id, object: primitiveCursor, instance: instanceCursor, date: new Date()})
+}
