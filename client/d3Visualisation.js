59efa80de83ed64153cab558825dbc71543c630e
Devian <info@devian.tk>
2016-02-15 13:33:41 +0100

    fix rotation bug

--- client/d3Visualisation.js
+++ client/d3Visualisation.js
@@ -13,7 +13,19 @@ Template.body.helpers({
 			boxId+objectIndex.toString(16)+instanceIndex.toString(16),
 			{sort: {date: -1}}
 		)
-	}
+	},
+
+	horizontalCenter(polygon) {
+		const xCoordinates = polygon.coordinates.map(coordinates => coordinates[0])
+		return (Math.min(...xCoordinates) + Math.max(...xCoordinates)) / 2
+	},
+
+	verticalCenter(polygon) {
+		const xCoordinates = polygon.coordinates.map(coordinates => coordinates[1])
+		return (Math.min(...xCoordinates) + Math.max(...xCoordinates)) / 2
+	},
+
+	degrees: radians => radians/Math.PI*180
 })
 
 /**
